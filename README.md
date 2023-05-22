ca28e1c6aad9536b272c280b56fee1b76b4deb12
troydeville <tsndeville@gmail.com>
2023-05-21 19:41:16 -0500

    2023.5.21.17.30-install.py
    While waiting on DL did an install file that is not necessary at all but idk.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Casual LLM
+
+## Installation
+
+### Install with the CLI:
+```bash
+python install.py conda env_name
+```
+
+#### or
+
+```bash
+python install.py pip
+```
---
+++ install.py
@@ -0,0 +1,67 @@
+import sys
+from utils import *
+
+def install():
+    # args
+    args = sys.argv
+    f_arg = None
+    if len(args) > 1: f_arg = args[1]
+    conda_env = None
+    if len(args) > 2: conda_env = args[2]
+
+    # stuff
+    title = colorstr("bright_blue", "+ Casual LLM Installer")
+    brks = ["{", "}"]
+
+    # packages required
+    packages = ["transformers"]
+    
+    # error messages
+    no_arg_error = colorstr("cyan", f'Use `conda {brks[0]}env_name{brks[1]}` or `pip` for argument\n')
+    conda_env_error = colorstr("cyan", f"`python install.py conda {brks[0]}env_name{brks[1]}`")
+    e_prefix = colorstr('bright_red', 'ERROR ❌️  ')
+
+    # title
+    LOGGER.info(f'{title}\n')
+
+    # Installation with pip
+    if f_arg == 'pip':
+        try:
+            # pip
+            LOGGER.info(f'\t{colorstr("bright_cyan", "Installing with `pip`.")}\n')
+
+            # package installations
+            for package in packages:
+                LOGGER.info(" ✅  " + colorstr("green", f"Installing {package}") + "\n")
+                os.system(f'pip install {package}')
+        except:
+            # error
+            LOGGER.error(f'{e_prefix}Package `{package}` failed to install using `pip`.\n', exc_info=1)
+    
+    # Installation with conda
+    elif f_arg == 'conda':
+        if conda_env is not None:
+            # conda with env
+            LOGGER.info(colorstr("cyan", f"- Installing with `conda` in `{conda_env}` environment\n"))
+            try:
+                # package installations
+                for package in packages:
+                    LOGGER.info(" ✅  " + colorstr("green", f"Installing {package}") + "\n")
+                    os.system(f"conda install -n {conda_env} {package}")
+                LOGGER.info("\n- " + colorstr("cyan", f"✅  Done.\n"))
+            except:
+                # error
+                LOGGER.error(f'{e_prefix}Package `{package}` failed to install for `{conda_env}` environment\n', exc_info=1)
+
+        # no conda env
+        else: LOGGER.error(f'{e_prefix}You are using conda without an environment specified.\n\n\tUse %s\n', conda_env_error)
+
+    # no args
+    else: LOGGER.error(f'\t{e_prefix}`arguments missing`\n\n\t{no_arg_error}')
+
+
+
+# Installs dependencies.
+# (more later on)
+if __name__ == "__main__":
+    install()

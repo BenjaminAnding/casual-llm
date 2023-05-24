4e63f1f3bceafbc2b9a0d93b48d600fac44bee75
troydeville <tsndeville@gmail.com>
2023-05-23 19:31:55 -0500

    2023.5.23-Pytorch + Extras

--- casual-llm.py
+++ casual-llm.py
@@ -1,35 +1,68 @@
+from pathlib import Path
 from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig
 from utils import *
 import torch
 from accelerate import init_empty_weights, load_checkpoint_and_dispatch
+# from huggingface_hub import hf_hub_download
 
 checkpoint = "EleutherAI/gpt-j-6B"
-LOGGER.info(f"{DEBUG_PREFIX}`loading config`")
-config = AutoConfig.from_pretrained(checkpoint)
+offload_dir = checkpoint
 
-LOGGER.info(f"{DEBUG_PREFIX}`model`")
-with init_empty_weights():
-    model = AutoModelForCausalLM.from_config(config)
+# # Offload directory for model running out of space.
+# Path(offload_dir).mkdir(parents=True, exist_ok=True)
 
-model.tie_weights()
-try:
-    model = load_checkpoint_and_dispatch(model, checkpoint, device_map="auto", offload_folder=checkpoint, no_split_module_classes=["GPTJBlock"]).to(DEVICE)
-except:
-    model.save_pretrained("EleutherAI/gpt-j-6B")
+# Check if the directory exists within the project
+model_is_local = directory_exists_within_project = directory_exists(checkpoint)
 
-tokenizer = AutoTokenizer.from_pretrained(checkpoint, padding_side="left")
-tokenizer.add_special_tokens({'pad_token':tokenizer.eos_token})
+if not model_is_local:
+    LOGGER.info(f"{WARNING_PREFIX}{colorstr('cyan', 'No pretrained config from')} %s", checkpoint)
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint, padding_side="left")
+    LOGGER.info(f"{colorstr('yellow', 'Loading model into memory.')}")
+    model = AutoModelForCausalLM.from_pretrained(checkpoint).to(DEVICE)
+    LOGGER.info(f"{colorstr('yellow', 'Saving model and tokenizer to drive storage.')}")
+    tokenizer.save_pretrained(checkpoint)
+    model.save_pretrained(checkpoint)
     
-# tokenizer.save_pretrained("EleutherAI/gpt-j-6B")
+    LOGGER.info(f"{colorstr('green', 'Model and tokenizer loaded and saved.')}")
+    tokenizer.add_special_tokens({'pad_token':tokenizer.eos_token})
 
-torch.manual_seed(42)
+# try: weights_location = hf_hub_download("EleutherAI/gpt-j-6B", "pytorch_model.bin")
+# except: weights_location = checkpoint
+else:
+    try:
+        config = AutoConfig.from_pretrained(checkpoint)
+        LOGGER.info(f"{colorstr('yellow', 'Loading tokenizer from')} %s", checkpoint)
+        tokenizer = AutoTokenizer.from_pretrained(checkpoint, padding_side="left")
+        with init_empty_weights():
+            LOGGER.info(f'{colorstr("yellow", "loading model with config")}')
+            model = AutoModelForCausalLM.from_config(config)
+        model.tie_weights()
+        
+        LOGGER.info(f'{colorstr("yellow", "loading model from ")}%s', checkpoint)
+        model = load_checkpoint_and_dispatch(
+            model,
+            checkpoint,
+            device_map="auto",
+            offload_folder=offload_dir,
+            no_split_module_classes=["GPTJBlock"]
+        ).to(DEVICE)
+        
+        tokenizer.add_special_tokens({'pad_token':tokenizer.eos_token})
+        LOGGER.info(f'{colorstr("green", "model and tokenizer loaded from checkpoint.")}')
 
+    except Exception as e:
+        LOGGER.info(f"{WARNING_PREFIX}{e}")
+        exit()
 
-LOGGER.info(colorstr("cyan", "\n+ Casual LLM\n"))
+if WINDOWS: os.system("cls")
+else: os.system("clear")
+
+# Main
+LOGGER.info(colorstr("cyan", "\n+ Casual LLM"))
 
 def generate(prompt: list, length) -> str:
     model.eval()
-    for _ in range(4): print(RLPX)
+    
     prompt = "".join((x) for x in prompt)
     inputs = tokenizer(prompt, return_tensors="pt").to(DEVICE)
 
@@ -47,13 +80,15 @@ def generate(prompt: list, length) -> str:
 
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     generated_text = generated_text.replace(prompt, '')
+    generated_text = generated_text.replace('\n\n', '\n')
+    generated_text = generated_text.replace('\t', ' ')
+    generated_text = generated_text.lstrip()
 
     return generated_text
 
 # 1.
 def create_prompt():
-    {RLPX}
-    LOGGER.info(f"Type your prompt. To exit type '**' on its own line.")
+    LOGGER.info(f"\033[FType your prompt. To exit type '**' on its own line.")
     prompt = []
     user_input = ""
     while user_input != "**":
@@ -63,39 +98,49 @@ def create_prompt():
 
 # 2.
 def read_back(prompt):
-    print("="*p_int_length)
-    print(prompt)
+    LOGGER.info("="*p_int_length)
+    LOGGER.info(prompt)
 
 # 3.
 def generate_text(prompt: str) -> str:
     return generate(prompt, 96)
 
 def main_loop():
-    prompt = ""
     user_input = ""
     prompt = ""
     response = ""
     while True:
-        # main_interface()
         user_input = input(ITF_MAIN)
+        LOGGER.info(f'\033[F\033[F{" "*max_width}')
         if user_input == "4": break
-        elif user_input == "1":
+        elif user_input == "":
             prompt = create_prompt()
+            joined_prompt = ''.join(pro + ' ' for pro in prompt)
+            log_prompt('user', joined_prompt)
+            for _ in range(3): LOGGER.info(RLPX)
+            for _ in range(2): LOGGER.info('\033[F\033[F')
+            LOGGER.info(f"{colorstr('blue', 'You')}: {joined_prompt}")
             response = generate_text(prompt)
-            print("".join(pro + " " for pro in prompt))
-            print_limited_width(response, max_width)
+            log_prompt('Andel', response)
+            LOGGER.info(f"{colorstr('green', 'Andel')}: {get_limited_width_text(response, max_width)}\n")
         elif user_input == "2":
             prompt = create_prompt()
-            print('\t- ', prompt)
-            # read_back(prompt)
+            LOGGER.info('\t- ', prompt)
         elif user_input == "3":
+            joined_prompt = ''.join(pro + ' ' for pro in prompt)
+            log_prompt('user', joined_prompt)
+            for _ in range(3): LOGGER.info(RLPX)
+            for _ in range(2): LOGGER.info('\033[F\033[F')
+            LOGGER.info(f"{colorstr('blue', 'You')}: {joined_prompt}")
             response = generate_text(prompt)
-            print("".join(pro + " " for pro in prompt))
-            print_limited_width(response, max_width)
+            log_prompt('Andel', response)
+            LOGGER.info(f"{colorstr('green', 'Andel')}: {get_limited_width_text(response, max_width)}\n")
         else:
-            print(RLPX)
-            print(RLPX)
-            LOGGER.info(f"{colorstr('bright_red', ' ❌️  invalad choice')}")
+            LOGGER.info(f"\033[F{colorstr('bright_red', ' ❌️  invalad choice')}")
+            LOGGER.info(input("Enter to Continue ↵ "))
+            for _ in range(4): LOGGER.info(f'\033[F\033[F{" "*max_width}')
                 
 if __name__ == '__main__':
+    clear_console()
     main_loop()
+    clear_console()
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import logging
 import platform
@@ -7,35 +8,27 @@ import torch.mps
 import textwrap
 
 
-class GPU:
-    def __init__(self, is_init_device=False, empty_cache=False) -> None:
-        self.gpu_allocation = {}
-        self.is_cuda = torch.cuda.is_available()
-        self.is_mps = torch.has_mps
-        self.device_init = torch.device(f'mps') if self.is_mps else torch.device(f'cuda') if self.is_cuda else torch.device(f'cpu')
-        if is_init_device and not self.is_cuda and not self.is_mps: torch.device(f'cpu')
-        if self.is_mps:
-            gpu_mem_alloc = torch.mps.current_allocated_memory() / (1024 ** 3)  # Convert bytes to gigabytes
-            gpu_mem_cached = torch.mps.driver_allocated_memory() / (1024 ** 3) - gpu_mem_alloc  # Convert bytes to gigabytes
-            self.gpu_allocation[f'GPU{0}'] = {'name': 'MPS', 'memory_allocated': f'{gpu_mem_alloc:.2f}G', 'memory_reserved': f'{gpu_mem_cached:.2f}G'}
-        if self.is_cuda: 
-            for i in range(torch.cuda.device_count()):
-                cuda_device = torch.device(f'cuda:{i}')
-                torch.cuda.set_device(cuda_device)
-                gpu_mem_alloc = torch.cuda.memory_allocated() / (1024 ** 3)  # Convert bytes to gigabytes
-                gpu_mem_cached = torch.cuda.memory_reserved() / (1024 ** 3)  # Convert bytes to gigabytes
-                cuda_name = torch.cuda.get_device_name(cuda_device)
-                self.gpu_allocation[f'GPU{i}'] = {'name': cuda_name, 'memory_allocated': f'{gpu_mem_alloc:.2f}G','memory_reserved': f'{gpu_mem_cached:.2f}G'}
-        self.gpu_mem_description = f"{gpu_mem_alloc:.2f}G/{gpu_mem_cached:.2f}G" if self.is_cuda or self.is_mps else f"0.00G/0.00G"
-        if empty_cache or is_init_device:
-            if self.is_cuda: torch.cuda.empty_cache()
-            if self.is_mps: torch.mps.empty_cache()
+def get_working_directory(): return os.getcwd()
 
-    def device(self) -> str:
-        return self.device_init
+def get_checkpoint_directory(checkpoint_folder): return os.path.join(get_working_directory(), checkpoint_folder)
 
-GPU_DEVICE = GPU(is_init_device=True)
-DEVICE = GPU_DEVICE.device()
+def directory_exists(directory_name):
+    # Get the current working directory
+    current_dir = get_working_directory()
+
+    # Construct the full path to the directory within the project
+    directory_path = os.path.join(current_dir, directory_name)
+
+    # Check if the directory exists
+    if os.path.exists(directory_path):
+        return True
+    else:
+        return False
+    
+def clear_console():
+    if WINDOWS: os.system("cls")
+    else: os.system("clear")
+
 LOGGING_NAME = 'CASUAL LLM'
 VERBOSE = True
 MACOS, LINUX, WINDOWS = (platform.system() == x for x in ['Darwin', 'Linux', 'Windows'])  # environment booleans
@@ -110,13 +103,69 @@ LOGGER = logging.getLogger(LOGGING_NAME)  # define globally (used in train.py, v
 if WINDOWS and EMOJI_SAFE_LOGGING:  # emoji-safe logging
     LOGGER.addFilter(EmojiFilter())
 
-max_width = 128
+def log_prompt(who: str, msg: str):
+        """
+        Writes defined msg to the `conversations.txt` file.
 
-DEBUG_PREFIX = f"🪲  {colorstr('bright_yellow', 'DEBUG')} - "
+        Format: `[YYYY.mm.dd.HH.MM.SS] [type] [log]`
+
+        Args:
+            `who`: A string; applied before the `msg` arg; the content creator.
+            `msg`: A string; the main content.
+
+        Returns:
+            `Void`
+        """
+        # Open the file in append mode and create it if it doesn't exist
+        with open("conversations.txt", "a+") as f:
+            # Get the current date and time
+            now = datetime.now()
+
+            # Format the date and time as [YYYY.MM.DD.SS]
+            formatted_date_time = now.strftime("%Y.%m.%d.%H.%M.%S")
+            f.write(f'[{formatted_date_time}]\n{who}: ' + msg + "\n")
+
+max_width = 128
+WARNING_PREFIX = f"⚠️  {colorstr('bright_yellow', 'WARNING')} - "
+DEBUG_PREFIX = f"🪲  {colorstr('bright_black', 'DEBUG')} - "
 RLPX = '\033[F' + ' '*max_width + '\033[F' 
 p_int_length = 40
-ITF_MAIN = "[1. Prompt+Response] [2. Prompt+Review] [3. Get Response] [4. Exit]: "
 
-def print_limited_width(string, max_width):
-    wrapped_text = textwrap.fill(string, width=max_width)
-    print("\n", wrapped_text, "\n")
+
+
+# ITF_MAIN = " Enter to Start ↵ \n 2 - Prompt reviews\n 3 - Response to lastest prompt\n 4 - Exit\n : "
+ITF_MAIN = "\033[F- Enter to prompt ↵ -- exit with `4` - "
+
+
+class GPU:
+    def __init__(self, is_init_device=False, empty_cache=False) -> None:
+        self.gpu_allocation = {}
+        self.is_cuda = torch.cuda.is_available()
+        self.is_mps = torch.has_mps
+        self.device_init = torch.device(f'mps') if self.is_mps else torch.device(f'cuda') if self.is_cuda else torch.device(f'cpu')
+        if is_init_device and not self.is_cuda and not self.is_mps: torch.device(f'cpu')
+        if self.is_mps:
+            gpu_mem_alloc = torch.mps.current_allocated_memory() / (1024 ** 3)  # Convert bytes to gigabytes
+            gpu_mem_cached = torch.mps.driver_allocated_memory() / (1024 ** 3) - gpu_mem_alloc  # Convert bytes to gigabytes
+            self.gpu_allocation[f'GPU{0}'] = {'name': 'MPS', 'memory_allocated': f'{gpu_mem_alloc:.2f}G', 'memory_reserved': f'{gpu_mem_cached:.2f}G'}
+        if self.is_cuda: 
+            for i in range(torch.cuda.device_count()):
+                cuda_device = torch.device(f'cuda:{i}')
+                torch.cuda.set_device(cuda_device)
+                gpu_mem_alloc = torch.cuda.memory_allocated() / (1024 ** 3)  # Convert bytes to gigabytes
+                gpu_mem_cached = torch.cuda.memory_reserved() / (1024 ** 3)  # Convert bytes to gigabytes
+                cuda_name = torch.cuda.get_device_name(cuda_device)
+                self.gpu_allocation[f'GPU{i}'] = {'name': cuda_name, 'memory_allocated': f'{gpu_mem_alloc:.2f}G','memory_reserved': f'{gpu_mem_cached:.2f}G'}
+        self.gpu_mem_description = f"{gpu_mem_alloc:.2f}G/{gpu_mem_cached:.2f}G" if self.is_cuda or self.is_mps else f"0.00G/0.00G"
+        if empty_cache or is_init_device:
+            if self.is_cuda: torch.cuda.empty_cache()
+            if self.is_mps: torch.mps.empty_cache()
+
+    def device(self) -> str:
+        return self.device_init
+
+GPU_DEVICE = GPU(is_init_device=True)
+DEVICE = GPU_DEVICE.device()
+
+def get_limited_width_text(string, max_width):
+    return textwrap.fill(string, width=max_width)
